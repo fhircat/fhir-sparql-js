67f22957b7b31d36867996ba4dced40813951559
Eric Prud'hommeaux <eric+github@w3.org>
2023-06-26 22:07:05 +0200

    + index predicates

---
+++ fhir-sparql-js/lib/fhir-sparql.js
@@ -0,0 +1,44 @@
+const {Visitor: ShExVisitor} = require('./ShExVisitor');
+
+const Ns = {
+  fhir: 'http://hl7.org/fhir/',
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+};
+
+class PredicateToShapeDecl extends ShExVisitor {
+  constructor (ctor_args) {
+    super(ctor_args);
+    this.map = {};
+    this.curDecl = null;
+  }
+  visitShapeDecl(decl, ...args) {
+    this.curDecl = decl;
+    const ret = super.visitShapeDecl(decl, ...args);
+    this.curDecl = null;
+    return ret;
+  }
+  visitTripleConstraint(expr, ...args) {
+    if (this.curDecl === null)
+      throw new Error(`visiting ${JSON.stringify(expr)} while not in a ShapeDecl`);
+    if (!expr.predicate.startsWith(Ns.rdf) && [Ns.fhir + 'v', Ns.fhir + 'nodeRole'].indexOf(expr.predicate) === -1) {
+      if (!this.map[expr.predicate])
+        this.map[expr.predicate] = [];
+      this.map[expr.predicate].push(this.curDecl);
+    }
+    return null;
+  }
+  visitNodeConstraint(shape, ...args) { // don't bother visiting NodeConstraints
+    return null;
+  }
+}
+
+class FhirSparql {
+  constructor (shex) {
+    this.shex = shex;
+    const visitor = new PredicateToShapeDecl();
+    visitor.visitSchema(shex);
+    this.predicateToShapeDecl = visitor.map;
+  }
+}
+
+module.exports = {FhirSparql}
--- fhir-sparql-js/tests/fhir-sparql-test.js
+++ fhir-sparql-js/tests/fhir-sparql-test.js
@@ -5,6 +5,8 @@ const SparqlParser = new SparqlJs.Parser();
 const ShExParser = require("@shexjs/parser").construct();
 const Tests = __dirname;
 const Resources = Path.join(__dirname, '../../fhir-sparql-common/src/main/resources/');
+const FhirShEx = ShExParser.parse(File.readFileSync(Path.join(Resources, 'org/uu3/ShEx-mini-terse.shex'), 'utf-8'));
+const {FhirSparql} = require('../lib/fhir-sparql');
 
 describe('sparql-json', () => {
   it('should parse SPARQL', () => {
@@ -16,13 +18,17 @@ describe('sparql-json', () => {
   });
 
   it('should parse ShEx', () => {
-    const FhirShEx = ShExParser.parse(File.readFileSync(Path.join(Resources, 'org/uu3/ShEx-mini-terse.shex'), 'utf-8'));
     const refFhirShExObj = JSON.parse(File.readFileSync(Path.join(Tests, 'ShEx-mini-terse.json'), 'utf-8'));
     expect(FhirShEx).toEqual(refFhirShExObj);
   });
 
+  it('should index predicates', () => {
+    const rewriter = new FhirSparql(FhirShEx);
+    expect(rewriter.predicateToShapeDecl['http://hl7.org/fhir/item'].map(d => d.id)).toEqual([ 'Questionnaire', 'Questionnaire.item' ]);
+  });
+
   it('should translate obs-path', () => {
+    const rewriter = new FhirSparql(FhirShEx);
     const iQuery = SparqlParser.parse(File.readFileSync(Path.join(Tests, '../../notes/obs-pat.srq'), 'utf-8'));
-    const FhirShEx = ShExParser.parse(File.readFileSync(Path.join(Resources, 'org/uu3/ShEx-mini-terse.shex'), 'utf-8'));
   });
 });
